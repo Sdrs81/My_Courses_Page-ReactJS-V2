480aea4e450c17a01705f8526201a86886654c67
Serdar Demirtaş <serdardemirtas25@gmail.com>
2023-03-31 21:05:22 +0300

    Buttons' functions have been added

--- src/Courses.jsx
+++ src/Courses.jsx
@@ -5,13 +5,37 @@ function Courses({ courses, removeCourse }) {
   const [index, setIndex] = useState(0);
   const {content, title, price } = courses[index]; // Javascript feature 
   
+  const checkIndex = (index) =>{
+    if(index < 0){
+        return courses.length - 1;
+    }
+    if(index > courses.length - 1){
+        return 0;
+    }
+    return index;
+  }  
+
+  const prevCourse = () => {
+    setIndex((index) => {
+        let newIndex = index - 1;
+        return checkIndex(newIndex);
+    })
+  }
+
+  const nextCourse = () => {
+    setIndex((index) => {
+        let newIndex = index + 1;
+        return checkIndex(newIndex);
+    })
+  }
+
   return (
     <div className="courseMainDiv">
       <div>
         <h2>Kurslarım</h2>
       </div>
       <div className="cardDiv">
-        <button className="prevNext">
+        <button className="prevNext" onClick={prevCourse}>
             <FaChevronLeft/>
         </button>
         <div className="card">
@@ -21,7 +45,7 @@ function Courses({ courses, removeCourse }) {
           </div>
           <p>{content}</p>
         </div>
-        <button className="prevNext">
+        <button className="prevNext" onClick={nextCourse}>
             <FaChevronRight/>
         </button>
       </div>
